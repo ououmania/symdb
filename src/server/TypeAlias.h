614ffcc5a3c344643337b164d7b637b2a1fc4707
ououmania <287919708@qq.com>
2024-07-04 10:20:31 +0800

    wrap filesystem

--- src/server/TypeAlias.h
+++ src/server/TypeAlias.h
@@ -1,7 +1,16 @@
 #pragma once
 
 #include <boost/asio.hpp>
+
+#if USE_BOOST_FILESYSTEM
 #include <boost/filesystem.hpp>
+#else // USE_BOOST_FILESYSTEM
+#if __cplusplus < 201703L
+#error "std::filesystem requires c++17 or later"
+#endif // __cplusplus < 201703L
+#include <filesystem>
+#endif // USE_BOOST_FILESYSTEM
+
 #include <memory>
 #include <set>
 #include <string>
@@ -10,18 +19,10 @@
 
 namespace symdb {
 
-// std::experimental::filesystem has no relative()
-// std::filesystem has no canonical() with a bas path
-// boost::filesystem is the most proper one at present.
-namespace filesystem = boost::filesystem;
-
 using AsioStream = boost::asio::posix::stream_descriptor;
 using AsioStreamPtr = std::unique_ptr<boost::asio::posix::stream_descriptor>;
 using StringVec = std::vector<std::string>;
 using StringVecPtr = std::shared_ptr<StringVec>;
-using fspath = filesystem::path;
-using FsPathVec = std::vector<fspath>;
-using FsPathSet = std::set<fspath>;
 
 template <class T>
 using RawPointerWrap = std::shared_ptr<typename std::remove_pointer<T>::type>;
@@ -30,22 +31,29 @@ template <class T, class Deleter>
 using UniqueRawPointerWrap =
     std::unique_ptr<typename std::remove_pointer<T>::type, Deleter>;
 
-#if USE_STD_FILESYSTEM
-inline time_t last_wtime(const fspath &path) {
-  auto ftime = filesystem::last_write_time(path);
-  return decltype(ftime)::clock::to_time_t(ftime);
-  ;
-}
-
-inline std::ostream &operator<<(std::ostream &osm, const fspath &path) {
-  return osm << path.string();
-}
-
-#else
-inline time_t last_wtime(const fspath &path) {
+#if USE_BOOST_FILESYSTEM
+namespace filesystem = boost::filesystem;
+inline time_t last_wtime(const filesystem::path &path) {
   return filesystem::last_write_time(path);
 }
 
-#endif
+#else // USE_BOOST_FILESYSTEM
+namespace filesystem = std::filesystem;
+inline time_t last_wtime(const filesystem::path &path) {
+  using std::chrono;
+  const auto ftime = filesystem::last_write_time(path);
+  const auto system_time = clock_cast<system_clock>(ftime);
+  return system_clock::to_time_t(system_time);
+}
+
+inline std::ostream &operator<<(std::ostream &osm, const filesystem::path &path) {
+  return osm << path.string();
+}
+
+#endif // USE_BOOST_FILESYSTEM
+
+using fspath = filesystem::path;
+using FsPathVec = std::vector<fspath>;
+using FsPathSet = std::set<fspath>;
 
 }  // namespace symdb
